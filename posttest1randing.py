cabb2766023d2eee2604da15e27e39a2c8815454
Novandra7 <awangarissaputra@gmail.com>
2023-01-07 18:21:36 +0800

    posttest 1 randing

---
+++ posttest1randing.py
@@ -0,0 +1,37 @@
+import os
+
+def pemisah ():
+    for i in range(len(ListKu)):
+        if type (ListKu[i]) == int:
+            listint.append(ListKu[i])
+        else:
+            listkosong.append(ListKu[i])
+
+def partition(l, bwh, atas):
+  pivot = l[bwh]
+  pos_batas = bwh+1
+  for j in range(bwh+1,atas):
+    if l[j] < pivot:
+      l[pos_batas],l[j]=l[j],l[pos_batas]
+      pos_batas += 1
+  l[pos_batas-1],l[bwh] = l[bwh],l[pos_batas-1]
+  return pos_batas
+
+def quicksort(l, bwh, atas):
+  if atas <= bwh:
+    return
+  q = partition(l, bwh, atas)
+  quicksort(l, bwh, q-1)
+  quicksort(l, q, atas)
+  return l
+
+
+ListKu = [12, 1, [22, 3, [8, 14]], 2, 6, [11], 90]
+listkosong = []
+listint = []
+
+os.system('cls')
+pemisah()
+quicksort(listint, 0, len(listint)-1)
+print("JAWABAN NO 1 =")
+print(listint + listkosong)
