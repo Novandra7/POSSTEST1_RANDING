b7182b91b19d569a4419dccb5c967496283f6307
Awang Muhammad Novandra Arissaputra <122107967+Novandra7@users.noreply.github.com>
2023-01-09 14:18:16 +0800

    posttest 1 randing

--- posttest1randing.py
+++ posttest1randing.py
@@ -1,39 +1,50 @@
 # Nama : Awang Muhammad Novandra Arissaputra
 # Kelas : Sistem Informasi A 2022
 
-import os
+def seperate(item):
+  global result
+  result = []
+  for variable in item:
+      if isinstance(variable, list):
+          for i in variable:
+              if isinstance(i, list):
+                  for a in i :
+                      # print(int(a))
+                      result.append (int (a))
+              elif isinstance(i, int) :
+                  # print(i)
+                  result.append (i)
+      else :
+          # print(variable)
+          result.append(variable)
+  urutin(result)
 
-def pemisah ():
-    for i in range(len(ListKu)):
-        if type (ListKu[i]) == int and list:
-            listint.append(ListKu[i])
-        else:
-            listkosong.append(ListKu[i])
+def urutin (array):
+      if len(array) > 1:
+        r = len(array)//2
+        l = array[:r]
+        m = array[r:]
+        urutin(l)
+        urutin(m)
+        i = j = k = 0
+        while i < len(l) and j < len(m):
+            if l[i] < m[j]:
+                array[k] = l[i]
+                i += 1
+            else:
+                array[k] = m[j]
+                j += 1
+            k += 1
+        while i < len(l):
+            array[k] = l[i]
+            i += 1
+            k += 1
+        while j < len(m):
+            array[k] = m[j]
+            j += 1
+            k += 1
 
-def partition(l, bwh, atas):
-  pivot = l[bwh]
-  pos_batas = bwh+1
-  for j in range(bwh+1,atas):
-    if l[j] < pivot:
-      l[pos_batas],l[j]=l[j],l[pos_batas]
-      pos_batas += 1
-  l[pos_batas-1],l[bwh] = l[bwh],l[pos_batas-1]
-  return pos_batas
-
-def quicksort(l, bwh, atas):
-  if atas <= bwh:
-    return
-  q = partition(l, bwh, atas)
-  quicksort(l, bwh, q-1)
-  quicksort(l, q, atas)
-  return l
-
-
-ListKu = [12, 1, [22, 3, [8, 14]], 2, 6, [11], 90]
-listkosong = []
-listint = []
-
-os.system('cls')
-pemisah()
-quicksort(listint, 0, len(listint)-1)
-print("Setelah diurutkan : ", listint +  listkosong)
+variable = [12, 1, [22, 3, [8, 14]], 2, 6, [11], 90]
+seperate(variable)
+print("sebelum diurutkan : ",variable)
+print("setelah diurutkan : ",result)
